3a3737b85b6d257d65476f03b887b7297a293530
jorge <jorge@senin.org>
2015-12-04 17:50:50 +0100

    upload a base64 image

--- Debate/lib/twitterWorker.js
+++ Debate/lib/twitterWorker.js
@@ -1,5 +1,6 @@
 var Twitter = require('twitter');
 var config = require('config');
+var fs = require('fs');
 
 var client = new Twitter({
   consumer_key: config.get('TWITTER_KEYS').CONSUMER_KEY,
@@ -16,3 +17,29 @@ exports.postTweet = function (message, image, callback) {
     callback(error, tweet, response);
   });
 };
+
+exports.prepareAnImageAsBase64 = function( file, callback ){
+    var options ={flags: 'r', encoding: 'base64'} ;
+
+    fs.readFile( file , options, function( err, data ){
+        callback( err, data );
+    });
+};
+
+exports.uploadImage = function( file, callback ){
+    
+    this.prepareAnImageAsBase64( file , function( err, base64Image ){
+
+        if(err) console.log(err);
+
+        client.post('media/upload', {media_data: base64Image},  function(err, tweet, response){
+            if(err) console.log(err);
+
+            console.log("Just twitted: " + tweet);  // Tweet body.
+            console.log("Response from twitter: " + response);  // Raw response object.
+            callback(err , tweet);
+
+        });
+
+    });
+}
--- Debate/spec/lib/twitterWorkerSpec.js
+++ Debate/spec/lib/twitterWorkerSpec.js
@@ -9,4 +9,26 @@ describe('Twitter functionality', function () {
       });
   });
 
+
+  it('should return a white4x4.png base64 image when prepareimage', function(done){
+        var file = './spec/lib/white4x4.png';
+        var fixture = 'iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3wwEEAQDhG1JwgAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAAUSURBVAjXY/z//z8DDDAxIAHcHACWbgMF2hiSxQAAAABJRU5ErkJggg==';
+
+        twitter.prepareAnImageAsBase64(file, function(err, data){
+            expect(data).toEqual( fixture );
+            done();
+        });
+
+  });
+
+  it('should upload an image to twitter', function(done){
+
+      var file = './spec/lib/white4x4.png';
+
+      twitter.uploadImage( file, function(err, data){
+          expect(data.media_id).toBeDefined();
+          done();
+      });
+  });
+
 });
