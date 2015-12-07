ad800057780cd458e09327f28a2f36d84c16216c
Algunenano <algunenano@gmail.com>
2015-12-07 01:22:08 +0100

    Simplify twitterWorker

--- Debate/lib/twitterWorker.js
+++ Debate/lib/twitterWorker.js
@@ -1,88 +1,65 @@
 var Twitter = require('twitter');
-var Twit = require('twit')
 var config = require('config');
-var fs = require('fs');
-var _ = require('underscore');
 
-var T = new Twit({
-  consumer_key: config.get('TWITTER_KEYS').CONSUMER_KEY,
-  consumer_secret: config.get('TWITTER_KEYS').CONSUMER_SECRET,
-  access_token: config.get('TWITTER_KEYS').ACCESS_TOKEN_KEY,
-  access_token_secret: config.get('TWITTER_KEYS').ACCESS_TOKEN_SECRET
-});
-
-var client = new Twitter({
+var twitter = new Twitter({
   consumer_key: config.get('TWITTER_KEYS').CONSUMER_KEY,
   consumer_secret: config.get('TWITTER_KEYS').CONSUMER_SECRET,
   access_token_key: config.get('TWITTER_KEYS').ACCESS_TOKEN_KEY,
   access_token_secret: config.get('TWITTER_KEYS').ACCESS_TOKEN_SECRET
 });
 
-exports.tweet = function ( message, options, callback ){
-  var defaultOptions =  { status: message } ;
-
-  options = _.extend( defaultOptions, options );
-  client.post('statuses/update', options ,  function(err, tweet){
-    if(err) console.log(err);
-    callback(err, tweet );
-  });
-
+/**
+ * Posts a new status in the user account
+ * @param {type} message - Message to be posted
+ * @param {type} options - Options to be used, such as media
+ * @param {type} callback (error)
+ * @returns {undefined}
+ */
+exports._tweetStatus = function (message, options, callback){
+    if (!message || !options || !callback) {
+        callback ("Invalid parameters");
+        return;
+    }
+    options.status = message;
+    console.log("Tweeting: " + JSON.stringify(options));
+    twitter.post('statuses/update', options,  function(error, tweet){
+        callback(error);
+    });
 };
 
+/**
+ * Posts media to Twitter to be used later
+ * @param {type} image - Raw image
+ * @param {type} callback (error, twitterMediaID
+ * @returns {undefined}
+ */
+exports._tweetMedia = function(image, callback){
+    twitter.post('media/upload', {media: image},  function(error, media, response){
+        callback(error, media.media_id_string);
+    });
+};
+
+/**
+ * Post a new Status in the user account
+ * @param {type} message - Status message
+ * @param {type} image - Image to be posted. Use undefined or raw image
+ * @param {type} callback (error)
+ * @returns {undefined}
+ */
 exports.postTweet = function (message, image, callback) {
-  var options  = {};
+    var self = this;
+    var options  = {};
 
-  if ( image ){
-    var that = this ;
-    this.uploadImage(image, function(err, imageTweet){
-      if ( err ) throw err;
-      options.media_ids = imageTweet.media_id_string;
-      return that.tweet( message, options , callback );
-    });
-    return ;
-  }
-  this.tweet( message, options, callback );
-};
-
-exports.prepareAnImageAsBase64 = function( file, callback ){
-  var options ={flags: 'r', encoding: 'base64'} ;
-
-  fs.readFile( file , options, function( err, data ){
-    var data = fs.readFileSync( file, options )
-    return callback( err, data );
-  });
-};
-
-exports.uploadImage = function( file, callback ){
-
-  this.prepareAnImageAsBase64( file , function( err, base64Image ){
-
-    if(err) console.log(err);
-
-    client.post('media/upload', {media_data: base64Image},  function(err, tweet, response){
-      if(err) console.log(err);
-
-      console.log("Just twitted: " + tweet);  // Tweet body.
-      console.log("Response from twitter: " + response);  // Raw response object.
-      callback(err , tweet);
-
-    });
-
-  });
-};
-
-exports.tweetWithMedia = function(text, data, callback) {
-  var b64content = data;
-
-  // first we must post the media to Twitter
-  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-
-    // now we can reference the media and post a tweet (media will attach to the tweet)
-    var mediaIdStr = data.media_id_string;
-    var params = { status: text, media_ids: [mediaIdStr] }
-
-    T.post('statuses/update', params, function (err, data, response) {
-      callback(err, data);
-    });
-  });
+    if (!image) {
+        self._tweetStatus(message, options, callback);
+    } else {
+        self._tweetMedia(image, function (error, mediaString) {
+            if (error) {
+                callback(error);
+                return;
+            }
+            options.media_ids = mediaString;
+            self._tweetStatus(message, {"media_ids" : mediaString}, callback);
+        });
+    }
 };
