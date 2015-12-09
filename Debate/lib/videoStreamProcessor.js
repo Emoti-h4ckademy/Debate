7b9b35ef708a65ba446f5d62451bc6c481334165
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2015-12-09 21:41:31 +0100

    update source for 8tv

--- Debate/lib/videoStreamProcessor.js
+++ Debate/lib/videoStreamProcessor.js
@@ -3,9 +3,10 @@ var exec = require('child_process').exec,
 var mkdirp = require('mkdirp');
 
 var laSextaStreamURL = "http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8";
+var _8tvStreamURL = "rtmp://91.121.143.193:1935/liverepeater/8TV/8aldia-directe";
 var secondsBetweenCaptures = 3;
 var destinationFolder = "./snapshots";
-var ffmpegCommand = "ffmpeg -i "+laSextaStreamURL+" -vf fps=1/"+secondsBetweenCaptures+" "+destinationFolder+"/out%d.png";
+var ffmpegCommand = "ffmpeg -i "+_8tvStreamURL+" -vf fps=1/"+secondsBetweenCaptures+" "+destinationFolder+"/out%d.png";
 
 
 mkdirp(destinationFolder, function (error) {
