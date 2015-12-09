336ade743710bbb3c66b236deb845a2cb3d7416e
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2015-12-09 23:44:05 +0100

    capture jpeg images instead of png

--- Debate/lib/videoStreamProcessor.js
+++ Debate/lib/videoStreamProcessor.js
@@ -3,9 +3,10 @@ var exec = require('child_process').exec,
 var mkdirp = require('mkdirp');
 
 var laSextaStreamURL = "http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8";
+var la1StreamURL = "http://iphonelive.rtve.es/LA1_LV3_IPH/LA1_LV3_IPH.m3u8";
 var secondsBetweenCaptures = 3;
 var destinationFolder = "./snapshots";
-var ffmpegCommand = "ffmpeg -i "+laSextaStreamURL+" -preset ultrafast -vf fps=1/"+secondsBetweenCaptures+" "+destinationFolder+"/out%d.png";
+var ffmpegCommand = "ffmpeg -i "+la1StreamURL+" -preset ultrafast -vf fps=1/"+secondsBetweenCaptures+" "+destinationFolder+"/out%d.jpeg";
 
 
 mkdirp(destinationFolder, function (error) {
