63d3bcf1889a1d68efba0bba1be2db6be2bc3774
Algunenano <algunenano@gmail.com>
2015-12-11 16:07:42 +0100

    GetFaces definition (not tested) and creation of several functions signatures

--- Debate/lib/oxfordFace.js
+++ Debate/lib/oxfordFace.js
@@ -4,12 +4,121 @@ var config = require('config'),
 /**
  * OxfordFace constructor.
  */
-
-function OxfordFace () {
-    this.apiKey         = config.get('OXFORD_EMOTION_API_KEY');
-    this.faceBaseUrl    = config.get('OXFORD_EMOTION_URL');
-    this.personBaseUrl  = config.get('OXFORD_PERSON_URL');
-    
+function OxfordFace() {
+    this.apiKey         = config.get('OXFORD_FACE_API_KEY');
+    this.faceBaseUrl    = config.get('OXFORD_FACE_DETECTION_URL');
+    this.personBaseUrl  = config.get('OXFORD_PERSON_URL');  
 }
+/*
+ * Good response body: As we do not ask for any options
+ * [
+              {
+              "faceId": "c5c24a82-6845-4031-9d5d-978df9175426",
+              "faceRectangle": {
+              "top": 54,
+              "left": 394,
+              "width": 78,
+              "height": 78
+              }
+    ]
+ */
+
+OxfordFace.prototype._parseGetFaceResponse = function (response, callback) {
+    var faces;
+    var error;
+    
+    if (!response) {
+        callback ("Empty response");
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        error = response.statusMessage;
+        var message;
+        var code;
+        try {
+            code = JSON.parse(response.body).code;
+            message = JSON.parse(response.body).message;
+            error += "("+message+"): " + code;
+        } catch (error) {
+            message = response.body;
+            error += ": " + message;
+        }
+    } else {
+        error = false;
+        faces = response.body;
+    }
+
+    callback (error, faces);
+};
+
+OxfordFace.prototype._getFaces = function (binaryImage, callback) {
+    var self = this;
+    var oxfordContentType = "application/octet-stream";
+    
+    var urlPlusOptions = self.faceBaseUrl; //No extra options
+
+    return request({
+        url: urlPlusOptions,
+        method: "POST",
+        json: false,
+        body: binaryImage,
+        headers: {
+                "content-type" : oxfordContentType,
+                "Ocp-Apim-Subscription-Key" : self.apiKey
+            }
+        }, function (error, response, body) {
+            if (error) {
+                callback ("Couldn't reach Oxford Service server");
+            }
+            else {
+                self._parseGetFaceResponse(response, callback);
+            }
+        });
+    
+};
+
+/**
+ * Analyzes an image (binary) calling oxford API
+ * @param {type} binaryImage
+ * @param {type} callback
+ * @returns {undefined}
+ */
+OxfordFace.prototype.detectFaces = function (binaryImage, callback) {
+    var self = this;
+    
+    if (!binaryImage) {
+        callback("Empty image");
+        return;
+    }
+    
+    self._getFaces(binaryImage, callback);
+};
+
+OxfordFace.prototype.createPerson = function (/*parameteres*/callback) {
+    
+    callback (/*error, oxfordPersonID*/true);
+};
+
+OxfordFace.prototype.createProject = function (/*parameteres*/callback) {
+    
+    callback (/*error, oxfordPersonGroupID*/true);
+};
+
+OxfordFace.prototype.trainProject = function (/*parameteres*/callback) {
+    
+    callback (/*error, status*/true);
+};
+
+OxfordFace.prototype.checkProjectStatus = function (/*parameteres*/callback) {
+    
+    callback (/*error, status*/true);
+};
+
+OxfordFace.prototype.identifyPersona = function (/*parameteres*/callback) {
+    
+    callback (/*error, persona*/true);
+};
+
 
 module.exports = new OxfordFace();
