4e35e001c60ce60c4da7280842f8ed741fb73cf4
carlosbkm <carlos.garcia.mejias@gmail.com>
2015-12-12 12:54:49 +0100

    Fixed project controller

--- Debate/controllers/project.js
+++ Debate/controllers/project.js
@@ -1,68 +1,68 @@
 /**
- * Training constructor
- * @returns {Training}
+ * Project constructor
+ * @returns {Project}
  */
-function Training() {
-    this.trainingDB  = require('../models/training');
+function Project() {
+    this.projectDB  = require('../models/project');
 }
 
-Training.prototype.getTrainings = function (callback) {
-    this.trainingDB.find().
+Project.prototype.getProjects = function (callback) {
+    this.projectDB.find().
             exec(callback);
 };
 
-Training.prototype.getTrainingById = function (callback) {
-    this.trainingDB.findById().
+Project.prototype.getProjectById = function (callback) {
+    this.projectDB.findById().
             exec(callback);
 };
 
-Training.prototype.create = function (name, callback) {
+Project.prototype.create = function (name, callback) {
     var self = this;
 
-    var training = new self.trainingDB({
+    var project = new self.projectDB({
         name:   name,
         trainedStatus: "no"
     });
 
-    training.save(function (error, data) {
+    project.save(function (error, data) {
        if (error) {
-           console.log("Training save ("+name+") ERROR:" + error);
+           console.log("Project save ("+name+") ERROR:" + error);
        } else {
-           console.log("Training save ("+name+") success");
+           console.log("Project save ("+name+") success");
        }
        callback (error, data);
     });
 };
 
-Training.prototype._generatePersonGroupID = function (trainingID, callback) {
+Project.prototype._generatePersonGroupID = function (projectID, callback) {
 
     callback(/*error, trainedStatus*/true);
 };
 
-Training.prototype.train = function (trainingID, callback) {
+Project.prototype.train = function (projectID, callback) {
 
     callback(/*error, updatedDocument*/true);
 };
 
-Training.prototype.getTrainedStatus = function (trainingID, callback) {
+Project.prototype.getTrainedStatus = function (projectID, callback) {
 
     callback(/*error, trainedStatus*/true);
 };
 
-Training.prototype.analyze = function (trainingID, callback) {
+Project.prototype.analyze = function (projectID, callback) {
 
     callback(/*error, trainedStatus*/true);
 };
 
-Training.prototype.getAnalysisStatus = function (trainingID, callback) {
+Project.prototype.getAnalysisStatus = function (projectID, callback) {
 
     callback(/*error, trainedStatus*/true);
 };
 
-Training.prototype.getEmotions = function (trainingID, callback) {
+Project.prototype.getEmotions = function (projectID, callback) {
 
     callback(/*error, trainedStatus*/true);
 };
 
 
-module.exports = new Training();
+module.exports = new Project();
