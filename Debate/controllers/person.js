ea96036efbaa5df2ba980d21fe30061df7f867a2
carlosbkm <carlos.garcia.mejias@gmail.com>
2015-12-12 20:32:32 +0100

    _getFacesIds added, needs to make it work

--- Debate/controllers/person.js
+++ Debate/controllers/person.js
@@ -1,20 +1,52 @@
+var fs = require('fs');
+var path = require('path');
+
 /**
  * Person constructor
  * @returns {Person}
  */
 function Person() {
-    this.personDB = require('../models/person');
+    this.personDB = require('../models/person').Person;
+    this.faceDB = require('../models/person').PersonImage;
+    this.oxfordFace = require('../lib/oxfordFace');
 }
 
-Person.prototype.create = function (projectID, name, directoryPath, callback) {
-    
+Person.prototype._getFacesIds = function (directoryPath, callback) {
+
+      var files = fs.readdirSync(directoryPath);
+      var faceIds = [];
+      var faceImg;
+      var error;
+      if (files) {
+        for (var i = 0; i < files.length; i++){
+          faceImg = fs.readFileSync(directoryPath + path.sep + files[i]);
+          person.oxfordFace.detectFaces(faceImg)
+          .then(function(face){
+            console.log("_getFacesIds Face Id: " + faceId);
+            faceIds.push(face[0].faceId);
+          }).catch(function (err) {
+            error = err;
+            console.log("Error detecting face of " + files[i] + ", error: " + JSON.stringify(err));
+          });
+        }
+      };
+  callback(error, faceIds);
+
+
+};
+
+Person.prototype.create = function (name, directoryPath, callback) {
+  person._getFacesIds(directoryPath, function(error, facesIds){
+      //TODO
+  });
+
     callback(/*error, generatedDocumet*/true);
 };
 
 Person.prototype.train = function (personID, callback) {
-    
+
     callback(/*error, updatedDocument*/true);
 };
 
 
-module.exports = new Person();
+var person = module.exports = new Person();
