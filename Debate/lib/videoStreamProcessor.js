140fe50e6d9d86aed1c1226c330c85ed647b13fb
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-01-11 22:00:50 +0100

    package.json

--- Debate/lib/videoStreamProcessor.js
+++ Debate/lib/videoStreamProcessor.js
@@ -3,9 +3,12 @@ var exec = require('child_process').exec,
 var mkdirp = require('mkdirp');
 
 var laSextaStreamURL = "http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8";
-var secondsBetweenCaptures = 3;
-var destinationFolder = "./snapshots";
-var ffmpegCommand = "ffmpeg -i "+laSextaStreamURL+" -preset ultrafast -vf fps=1/"+secondsBetweenCaptures+" "+destinationFolder+"/out%d.png";
+var video = "video/debateA9.mp4"
+var videCaraACara = "video/caraAcara.mp4";
+var videBalonOro = "video/BallondOr2015.mp4"
+var secondsBetweenCaptures = 1;
+var destinationFolder = "./snapshots/balonOro";
+var ffmpegCommand = "ffmpeg -i "+videBalonOro+" -preset ultrafast -vf fps=1/"+secondsBetweenCaptures+" "+destinationFolder+"/out%04d.jpeg";
 
 
 mkdirp(destinationFolder, function (error) {
